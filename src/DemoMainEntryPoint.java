4c0670acc1e6540b49a970dfb6d5c2a5ef835445
Satish Bharatiya <bharadwajss@gmail.com>
2023-07-22 20:44:15 +0530

    first commit

---
+++ src/DemoMainEntryPoint.java
@@ -0,0 +1,10 @@
+/**
+* @Author: Satish Bharatiya
+* Jul 22, 2023
+*/
+public class DemoMainEntryPoint {
+	public static void main(String [] args) {
+		System.out.println("welcome to git hub");
+	}
+
+}
